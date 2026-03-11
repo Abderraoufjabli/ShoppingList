428415834b5662e88130bdc8ea395bf348289e98
Abderraouf <jabli.abderraouf@gmail.com>
2026-03-12 00:38:58 +0100

    checkUI

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const itemForm = document.getElementById('item-form');
 const itemList = document.getElementById('item-list');
 const itemInput = document.getElementById('item-input');
+const clearButton = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+
+
+
+
 
 // function creation button
 
@@ -42,11 +48,65 @@ function addItem(e) {
     li.appendChild(text);
     li.appendChild(button);
     itemList.appendChild(li);
-    itemInput.value = '';    
+    itemInput.value = '';   
+    
+    checkUI();
 
 }
 
 // Event Listeners 
 
 itemForm.addEventListener('submit',addItem);
+itemList.addEventListener('click',removeItem);
+clearButton.addEventListener('click',clearItems);
+
+function removeItem(e){
+    if(e.target.classList.contains('fa-xmark')){
+         if(confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();
+         }
+        
+    }
+
+    checkUI();
+    
+}
+
+function clearItems(e){
+    
+
+    while(itemList.firstChild){
+        itemList.removeChild(itemList.firstChild)
+    }
+   
+
+    checkUI();
+    
+    
+}
+
+// check UI
+
+function checkUI(){
+    const items = itemList.querySelectorAll('li');
+    if(items.length === 0){
+        clearButton.style.display = 'none';
+        itemFilter.style.display = 'none';
+    }else
+     {
+        clearButton.style.display = 'block';
+        itemFilter.style.display = 'block';
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
 
